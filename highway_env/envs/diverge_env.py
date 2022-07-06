66d6c3470cfc8402450829b03d7cfbdfcdd1a148
ASaboor-Syed <98633649+ASaboor-Syed@users.noreply.github.com>
2022-07-06 15:02:07 -0400

    Update diverge_env.py

--- highway_env/envs/diverge_env.py
+++ highway_env/envs/diverge_env.py
@@ -76,18 +76,18 @@ class DivergeEnv(AbstractEnv):
         ends = [75, 40, 80, 150]  # Before, converging, merge, after
         c, s, n = LineType.CONTINUOUS_LINE, LineType.STRIPED, LineType.NONE
         y = [0, StraightLane.DEFAULT_WIDTH]
-        line_type = [[c, s], [s, s], [n, c]]
-        line_type_merge = [[c, s], [s, s], [n, s]]
+        line_type = [[c, s], [n, c]]
+        line_type_merge = [[c, s], [n, s]]
         for i in range(len(line_type)):
             net.add_lane("a", "b", StraightLane([0, i*StraightLane.DEFAULT_WIDTH], [sum(ends[:2]), i*StraightLane.DEFAULT_WIDTH], line_types=line_type[i]))
             net.add_lane("b", "c", StraightLane([sum(ends[:2]), i*StraightLane.DEFAULT_WIDTH], [sum(ends[:3]), i*StraightLane.DEFAULT_WIDTH], line_types=line_type_merge[i]))
             net.add_lane("c", "d", StraightLane([sum(ends[:3]), i*StraightLane.DEFAULT_WIDTH], [sum(ends), i*StraightLane.DEFAULT_WIDTH], line_types=line_type[i]))
 
         # Merging lane
-        amplitude = 3.25
+        amplitude = -3.25
         ljk = StraightLane([0, 6.5 + 4 + 4], [ends[0], 6.5 + 4 + 4], line_types=[c, c], forbidden=True)
         lkb = SineLane(ljk.position(ends[0], -amplitude), ljk.position(sum(ends[:2]), -amplitude),
-                       amplitude, np.pi / (ends[1]), np.pi / 2, line_types=[c, c], forbidden=True)
+                       amplitude, np.pi / (ends[1]), np.pi / 4, line_types=[c, c], forbidden=True)
         lbc = StraightLane(lkb.position(ends[1], 0), lkb.position(ends[1], 0) + [ends[2], 0],
                            line_types=[n, c], forbidden=True)
         net.add_lane("j", "k", ljk)
@@ -120,6 +120,6 @@ class DivergeEnv(AbstractEnv):
 
 
 register(
-    id='diverge-v1',
+    id='diverge-v0.1',
     entry_point='highway_env.envs:DivergeEnv',
 )
