5767e5ddfdcb5d8f7ff23c06ef02719b732fd713
ASaboor-Syed <98633649+ASaboor-Syed@users.noreply.github.com>
2022-07-14 13:58:21 -0400

    Update diverge_env.py

--- highway_env/envs/diverge_env.py
+++ highway_env/envs/diverge_env.py
@@ -81,20 +81,20 @@ class DivergeEnv(AbstractEnv):
         lanes = []
         for i in range(len(line_type)):
             lane_parts = []
-            lane_parts.append(StraightLane([0, i*StraightLane.DEFAULT_WIDTH], [sum(ends[:2]), i*StraightLane.DEFAULT_WIDTH], line_types=line_type[i]))
-            lane_parts.append(StraightLane([sum(ends[:2]), i*StraightLane.DEFAULT_WIDTH], [sum(ends[:3]), i*StraightLane.DEFAULT_WIDTH], line_types=line_type[i]))
+            lane_parts.append(StraightLane([0, i*StraightLane.DEFAULT_WIDTH], [ends[0], i*StraightLane.DEFAULT_WIDTH], line_types=line_type[i]))
+            lane_parts.append(StraightLane(ends[0], i*StraightLane.DEFAULT_WIDTH], [sum(ends[:2]), i*StraightLane.DEFAULT_WIDTH], line_types=line_type[i]))
             lanes.append(lane_parts)
             
         n_diverging = 1
         amplitude = 2.75
         
-        lanes[0].append(SineLane(lanes[0][1].position(ends[2], -amplitude), lanes[0][1].position(sum(ends[:3]), -amplitude),
+        lanes[0].append(SineLane(lanes[0][1].position(sum(ends[:2]), -amplitude), lanes[0][1].position(sum(ends[:3]), -amplitude),
                     amplitude, np.pi / (ends[2]), np.pi / 2, line_types=[c, c]))
-        lanes[1].append(SineLane(lanes[1][1].position(ends[2], amplitude), lanes[1][1].position(sum(ends[:3]), amplitude),
+        lanes[1].append(SineLane(lanes[1][1].position(sum(ends[:2]), amplitude), lanes[1][1].position(sum(ends[:3]), amplitude),
                     -amplitude, np.pi / (ends[2]), np.pi / 2, line_types=[c, c]))
         
         for lane in lanes:
-            lane.append(StraightLane(lane[2].position(ends[2], 0), lkb.position(ends[2], 0) + [ends[3], 0],
+            lane.append(StraightLane(lane[2].position(ends[3], 0), lkb.position(ends[3], 0) + [ends[3], 0],
                            line_types=[c, c])
 
         for l in lanes:
