305bc4237ef7b74efdcd31947bcfebc52dba72fd
ASaboor-Syed <98633649+ASaboor-Syed@users.noreply.github.com>
2022-07-19 23:12:40 -0400

    Update diverge_env.py

--- highway_env/envs/diverge_env.py
+++ highway_env/envs/diverge_env.py
@@ -120,9 +120,9 @@ class DivergeEnv(AbstractEnv):
         road.vehicles.append(ego_vehicle)
 
         other_vehicles_type = utils.class_from_path(self.config["other_vehicles_type"])
-        road.vehicles.append(other_vehicles_type(road, road.network.get_lane(("a", "b", 0)).position(90, 0), speed=29))
-        road.vehicles.append(other_vehicles_type(road, road.network.get_lane(("a", "b", 1)).position(70, 0), speed=31))
-        road.vehicles.append(other_vehicles_type(road, road.network.get_lane(("a", "b", 0)).position(5, 0), speed=31.5))
+        road.vehicles.append(other_vehicles_type(road, road.network.get_lane(("a", "b", 0)).position(90, 0), speed=29,target_lane_index=("b","c",np.random.randint(2))))
+        road.vehicles.append(other_vehicles_type(road, road.network.get_lane(("a", "b", 1)).position(70, 0), speed=31,target_lane_index=("b","c",np.random.randint(2))))
+        road.vehicles.append(other_vehicles_type(road, road.network.get_lane(("a", "b", 0)).position(5, 0), speed=31.5,target_lane_index=("b","c",np.random.randint(2))))
 
         self.vehicle = ego_vehicle
 
